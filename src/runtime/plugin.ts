02008d1b4a724added7682b40d6c7bea54463faa
Vsevolod Girenko <vs.girenko@mail.ru>
2023-12-20 20:57:57 +0300

    feat: redmine rest api client plugin
    Plugin implements repository pattern and provides resource registry for Nuxt application

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,5 +1,22 @@
-import { defineNuxtPlugin } from '#app'
+import { defineNuxtPlugin } from '#imports'
+import RedmineClient from '../runtime/api/client'
+import IssueResourceModule from './api/resources/issues'
+import ProjectResourceModule from './api/resources/projects'
+
+type ResourceModuleRegistry = {
+  issues: IssueResourceModule
+  projects: ProjectResourceModule
+}
 
 export default defineNuxtPlugin((nuxtApp) => {
-  console.log('Plugin injected by nuxt-redmine!')
+  const redmineBaseUrl = nuxtApp.$config.public.redmine.baseUrl
+  const redmineApiKey = nuxtApp.$config.redmineApiKey
+  const redmineClient = new RedmineClient(redmineBaseUrl, redmineApiKey)
+
+  const modules: ResourceModuleRegistry = {
+    issues: new IssueResourceModule(redmineClient),
+    projects: new ProjectResourceModule(redmineClient)
+  }
+
+  return { provide: { redmine: modules } }
 })
