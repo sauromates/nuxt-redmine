2cc39fead8eb950255d90550aaf35aea52c6caf0
Vsevolod Girenko <vs.girenko@mail.ru>
2023-12-29 15:53:43 +0300

    fix: api routes extensions

--- src/runtime/routes.ts
+++ src/runtime/routes.ts
@@ -5,44 +5,44 @@ export function defineRedmineApiRoutes(resolver: Resolver, resources: RedmineRes
   addServerHandler({
     route: '/api/redmine/issues/:id/watchers',
     method: 'post',
-    handler: resolver.resolve('./runtime/server/api/redmine/issues/[id].watchers.post.ts')
+    handler: resolver.resolve('./runtime/server/api/redmine/issues/[id].watchers.post')
   })
 
   addServerHandler({
     route: '/api/redmine/issues/:id/watchers/:user_id',
     method: 'delete',
-    handler: resolver.resolve('./runtime/server/api/redmine/issues/[id].watchers.[user_id].delete.ts')
+    handler: resolver.resolve('./runtime/server/api/redmine/issues/[id].watchers.[user_id].delete')
   })
 
   for (const resource of resources) {
     addServerHandler({
       route: `/api/redmine/${resource}`,
       method: 'get',
-      handler: resolver.resolve(`./runtime/server/api/redmine/${resource}/index.get.ts`)
+      handler: resolver.resolve(`./runtime/server/api/redmine/${resource}/index.get`)
     })
 
     addServerHandler({
       route: `/api/redmine/${resource}`,
       method: 'post',
-      handler: resolver.resolve(`./runtime/server/api/redmine/${resource}/index.post.ts`)
+      handler: resolver.resolve(`./runtime/server/api/redmine/${resource}/index.post`)
     })
 
     addServerHandler({
       route: `/api/redmine/${resource}/:id`,
       method: 'get',
-      handler: resolver.resolve(`./runtime/server/api/redmine/${resource}/[id].get.ts`)
+      handler: resolver.resolve(`./runtime/server/api/redmine/${resource}/[id].get`)
     })
 
     addServerHandler({
       route: `/api/redmine/${resource}/:id`,
       method: 'put',
-      handler: resolver.resolve(`./runtime/server/api/redmine/${resource}/[id].put.ts`)
+      handler: resolver.resolve(`./runtime/server/api/redmine/${resource}/[id].put`)
     })
 
     addServerHandler({
       route: `/api/redmine/${resource}/:id`,
       method: 'delete',
-      handler: resolver.resolve(`./runtime/server/api/redmine/${resource}/[id].delete.ts`)
+      handler: resolver.resolve(`./runtime/server/api/redmine/${resource}/[id].delete`)
     })
   }
 }
